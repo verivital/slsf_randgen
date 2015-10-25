07e57cdc4f2c5f40cbb0be61ab138f1e663222d5
Shafiul A Chowdhury <ishafiul@gmail.com>
2015-10-25 15:48:40 -0500

    Timeout is now handled

--- run_from_matlab.py
+++ run_from_matlab.py
@@ -2,24 +2,34 @@
 
 import subprocess
 import sys
+import os
+import signal
 
 TIMEOUT = 10
+
+p_randgen = None
 
 try:
 
     print('[x] Calling csmith')
-    subprocess.call('csmith --no-argc > randgen.c', shell=True)
+    p_cs = subprocess.call('csmith --no-argc > randgen.c', shell=True)
     print('[x] End csmith');
 
-    subprocess.call('gcc randgen.c -o randgen', shell=True)
+    p_gcc = subprocess.call('gcc randgen.c -o randgen', shell=True)
 
     # Does the program terminate?
 
-    subprocess.call('./randgen', shell=True, timeout=TIMEOUT )
+    p_randgen = subprocess.call('./randgen', timeout=TIMEOUT )
+    #print('Randgen p id: {0}'.format(p_randgen.pid))
 except Exception as e:
-    #print('Exception Occurred {0}'.format(e));
-    print('Exception Occurred, returning error code.');
+    print('Exception Occurred, returning error code: {0}'.format(e));
     sys.exit(100);
+finally:
+    #os.killpg(p_cs.pid, signal.SIGTERM) 
+    #os.killpg(p_gcc.pid, signal.SIGTERM) 
+    #os.killpg(p_randgen.pid, signal.SIGTERM) 
+    pass
+    
 
-print('[x] Returning from python script');
+print('[x] Returning successfully from python script');
 sys.exit(0)
