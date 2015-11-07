d29dabfe24fc42645198ab3649b1f9f69e86e85c
Shafiul A Chowdhury <ishafiul@gmail.com>
2015-11-06 18:34:04 -0600

    Adding sfunction variables into csmith checksum generation process

---
+++ ee_pre.c
@@ -0,0 +1,5 @@
+/* Following code will be place at the top of csmith generated code */
+
+static int g_slsf_in;
+static int g_slsf_out;
+
--- run_from_matlab.py
+++ run_from_matlab.py
@@ -9,11 +9,20 @@ TIMEOUT = 10
 
 p_randgen = None
 
+def copy_files():
+    filenames = ['randgen.c', 'ee_post.c']
+    with open('staticsfun.c', 'w') as outfile:
+        for fname in filenames:
+            with open(fname) as infile:
+                for line in infile:
+                    outfile.write(line)
+
+
 try:
 
     print('[x] Calling csmith')
-    p_cs = subprocess.call('csmith --no-argc > randgen.c', shell=True)
-    print('[x] End csmith');
+    p_cs = subprocess.call('csmith --easy-x --no-argc > randgen.c', shell=True)
+    print('[x] End csmith')
 
     p_gcc = subprocess.call('gcc randgen.c -o randgen', shell=True)
 
@@ -21,9 +30,13 @@ try:
 
     p_randgen = subprocess.call('./randgen', timeout=TIMEOUT )
     #print('Randgen p id: {0}'.format(p_randgen.pid))
+
+    # Copy randgen.c and ee_post.c together into one file
+
+    copy_files()
 except Exception as e:
-    print('Exception Occurred, returning error code: {0}'.format(e));
-    sys.exit(100);
+    print('Exception Occurred, returning error code: {0}'.format(e))
+    sys.exit(100)
 finally:
     #os.killpg(p_cs.pid, signal.SIGTERM) 
     #os.killpg(p_gcc.pid, signal.SIGTERM) 
